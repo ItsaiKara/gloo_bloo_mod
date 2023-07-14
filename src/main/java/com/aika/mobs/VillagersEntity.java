4b200bf2e741c3ea8b1f6181fcc11c6d1d3286dd
AiKa <eloytyf79@gmail.com>
2023-07-14 09:25:37 +0200

    Wip: Villager start

---
+++ src/main/java/com/aika/mobs/VillagersEntity.java
@@ -0,0 +1,27 @@
+package com.aika.mobs;
+
+import net.fabricmc.fabric.api.object.builder.v1.villager.VillagerProfessionBuilder;
+import net.minecraft.client.render.debug.VillageDebugRenderer.PointOfInterest;
+import net.minecraft.entity.EntityType;
+import net.minecraft.entity.passive.VillagerEntity;
+import net.minecraft.registry.Registry;
+import net.minecraft.registry.RegistryKey;
+import net.minecraft.util.Identifier;
+import net.minecraft.village.VillagerProfession;
+import net.minecraft.village.VillagerType;
+import net.minecraft.world.World;
+
+public class VillagersEntity  {
+
+    public static VillagerProfession registerVillagerProfession(String name, RegistryKey<PointOfInterest> type){
+        return Registry.register(Registry.VILLAGER_PROFESSION, name, new Identifier("gloo_bloo", name), type), VillagerProfessionBuilder.create().build();
+    }
+
+
+    public VillagersEntity(EntityType<? extends VillagerEntity> entityType, World world, VillagerType type) {
+
+    }
+
+    
+    
+}
